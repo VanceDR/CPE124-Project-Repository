3b66dc18617b2ae2aa11e8a2ba146a5749a170ff
jrfreyes <justinrupert.reyes@gmail.com>
2022-10-11 16:18:32 +0900

    Refactor sensor code
    - Move sensor watch outside socket code
    - Refactor some redundant code into function

--- app.js
+++ app.js
@@ -79,55 +79,63 @@ console.log("WebSocket is Running");
 
 // Initializing Socket.IO
 const io = socket(server); // Initialize socket at the server
-var peopleCount = 0; // Counter Variable
+let peopleCount = 0; // Counter Variable
+var startTime = DateTime.now(); // Start Time after Initializing
+
+function getRelativeTime() {
+  var endTime = DateTime.now(); // Takes current time
+  var changeTime = endTime.diff(startTime).toMillis(); // Subtracts current time to previous time
+  var relativeTime = DateTime.now().minus(changeTime).toRelative(); // Gets the relative time to previous time, i.e. 4 seconds ago
+  startTime = DateTime.now(); // Resets the current time
+  return relativeTime
+}
+
+
+const entering = () => {
+  // ---- For Time Last Updated when someone Entered ----
+  const relativeTime = getRelativeTime();
+
+  peopleCount++; // Increment Counter
+  
+  // -- end --
+  io.emit("message", peopleCount, relativeTime); // Emiting using Socket.IO to send data to HTML
+}
+
+const exiting = () => {
+  // ---- For Time Last Updated when someone Entered ----
+  const relativeTime = getRelativeTime();
+  
+  if (peopleCount > 0) { // If count is zero, remain at zero
+    peopleCount--; // Decrements the counter
+  }
+  
+  // -- end --
+  io.emit("message", peopleCount, relativeTime); // Emiting using Socket.IO to send data to HTML
+}
+
+// STARTING THE SENSORS
+entrySensor.watch((err,value) => {
+  if (err) exit(err); // If error, display error
+
+  if (value == 1){ 
+    entering() // If actiavted, emit a entering event
+  } else {
+    console.log('Person Entered') // if person passed, display that a person entered
+  }
+})
+
+exitSensor.watch((err,value) => {
+  if (err) exit(err); // If error, display error
+  
+  if (value == 1){
+    exiting()  // If actiavted, emit a entering event
+  } else {
+    console.log('Person Exited') // if person passed, display that a person entered
+  }
+})
+
 io.on("connection", (socket) => { // Listens for connection to the server
   console.log(`A connection is running @ ${socket.id}`); // Displays connection ID
-  var startTime = DateTime.now(); // Start Time after Initializing
-
-  // STARTING THE SENSORS
-  entrySensor.watch((err,value) => {
-    if (err) exit(err); // If error, display error
-    if (value == 1){ 
-      socket.emit('entering') // If actiavted, emit a entering event
-    } else {
-      console.log('Person Entered') // if person passed, display that a person entered
-    }
-  })
-
-  exitSensor.watch((err,value) => {
-    if (err) exit(err); // If error, display error
-    if (value == 1){
-      socket.emit('exiting')  // If actiavted, emit a entering event
-    } else {
-      console.log('Person Exited') // if person passed, display that a person entered
-    }
-  })
-
-  // WHEN SOMEONE ENTERS
-  socket.on("entering", () => {
-    // ---- For Time Last Updated when someone Entered ----
-    var endTime = DateTime.now(); // Takes current time
-    var changeTime = endTime.diff(startTime).toMillis(); // Subtracts current time to previous time
-    var relativeTime = DateTime.now().minus(changeTime).toRelative(); // Gets the relative time to previous time, i.e. 4 seconds ago
-    startTime = DateTime.now(); // Resets the current time
-    peopleCount++; // Increment Counter
-    // -- end --
-    io.emit("message", peopleCount, relativeTime); // Emiting using Socket.IO to send data to HTML
-  });
-
-  // WHEN SOMEONE EXITS
-  socket.on("exiting", () => {
-    // ---- For Time Last Updated when someone Entered ----
-    var endTime = DateTime.now(); // Takes current time
-    var changeTime = endTime.diff(startTime).toMillis(); // Subtracts current time to previous time
-    var relativeTime = DateTime.now().minus(changeTime).toRelative(); // Gets the relative time to previous time, i.e. 4 seconds ago
-    startTime = DateTime.now(); // Resets the current time
-    if (peopleCount > 0) { // If count is zero, remain at zero
-      peopleCount--; // Decrements the counter
-    }
-    // -- end --
-    io.emit("message", peopleCount, relativeTime); // Emiting using Socket.IO to send data to HTML
-  });
 
   // SAVES CURRENT SINGLE DATA
   socket.on("currentData", (data, time) => {
