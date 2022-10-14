2b7c75a1f7e2b65ea281e72c6d03153c01f76535
VanceDelRosario <vancedelrosario@yahoo.com>
2022-10-14 11:53:42 +0800

    removed comments, added app.js without IO and appIO.js for with IO

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,23 +1,14 @@
-// Initial version
 var express = require("express"),
-  router = express.Router(), //#A
+  router = express.Router(), 
   resources = require("./../resources/model");
 
-router.route("/").get(function (req, res, next) {
-  //#B
-  res.send(resources.pi.sensors); //#C
+router.route("/pi").get(function (req, res, next) {
+  res.send(resources.pi.sensors);
 });
 
 router.route("/pir").get(function (req, res, next) {
-  //#D
   res.send(resources.pi.sensors.pir);
 });
 
-module.exports = router; //#F
+module.exports = router;
 
-//#A We require and instantiate an Express Router to define the path to our resources
-//#B Create a new route for a GET request on all sensors and attach a callback function
-//#C Reply with the sensor model when this route is selected
-//#D This route serves the passive infrared sensor
-//#E These routes serve the temperature and humidity sensor
-//#F We export router to make it accessible for "requirers" of this file
